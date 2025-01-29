f6fada37c143ea379c39afc121bd55875cfa709a
mirkotorrisi <mirko.torrisi92@gmail.com>
2025-01-29 23:17:35 +0100

    Add Hero and KeyFeatures components for showcasing destinations

---
+++ src/components/showcase/Hero.tsx
@@ -0,0 +1,19 @@
+const Hero = () => {
+  return (
+    <div className=" text-white py-24 px-4 container rounded-xl">
+      <div className="max-w-4xl mx-auto items-start">
+        <h1 className="text-5xl md:text-6xl font-bold mb-6 hero-title max-w-sm">
+          Explore the Wonders of the Universe
+        </h1>
+        <p className="text-xl md:text-2xl mb-8">
+          Embark on an extraordinary journey through the cosmos with us.
+          Discover breathtaking destinations, from the rings of Saturn to the
+          farthest galaxies. Click on a destination to start your adventure and
+          uncover the secrets of the universe.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
